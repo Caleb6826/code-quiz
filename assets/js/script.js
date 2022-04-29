3fc47acb2d43e34a94172a642dc082f7567d7a63
Caleb Barkman <calebbarkman57@gmail.com>
2022-04-28 21:08:34 -0400

    start

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+const quizdata = [
+    {
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    correct: ''
+    },
+    {
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    correct: ''
+    },
+    {
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    correct: ''
+    },
+    {
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    1: 'blah',
+    correct: ''
+    },
+    {
+    1: 'blah',
+    2: 'blah',
+    3: 'blah',
+    4: 'blah',
+        correct: ''
+        },
+                        
+]
+
+
